ddbe7f1d05b2125e3b482b69f3e09c7606c8cdf9
ekuginyte <ek222@st-andrews.ac.uk>
2022-12-07 23:48:06 +0000

    Fixes
    Moved the packages from this file to the global one. Other small fixes.

--- server.R
+++ server.R
@@ -1,136 +1,104 @@
-# Server file for the Shiny app
-packages <- c("shiny", "tidyverse", "robotstxt", "rvest", "maps", "ggmap", "ggplot2",
-              "ggiraph", "RColorBrewer", "reshape2", "shinyjs", "shinythemes", 
-              "shinydashboard", "sf", "rgdal", "leaflet", "shinyWidgets", "mgsub",
-              "data.table")
-
-installed_packages <- packages %in% rownames(installed.packages())
-
-if (any(installed_packages == FALSE)) {
-  install.packages(packages[!installed_packages])
-}
-
-# Packages loading
-invisible(lapply(packages, library, character.only = TRUE))
-library(shinythemes)
-
-# Current date
-lastSessionStart <- as.Date(strftime(Sys.time(), "%Y/%m/%d"))
-
-# Initial dataset
-map_data_choice <- "confirmed"
-TSData <- get.time.series.data()
-
-# Extract dates from the time series data
-dateOptions <- names(TSData[-1])
-
 ### Server
 server <- function(input, output){
-  
+
  observeEvent(
    
    # Refresh when clicked
     input$refresh, {
       
       # Initial dataset
-      map_data_choice <- "confirmed"
-      TSData <- get.time.series.data()
+      #map_data_choice <- "confirmed"
       
+      # Extract all possible values for dates
+      TSData <- get.time.series.data()
       # Extract dates from the time series data
-      dateOptions <- names(TSData[-1])  
+      dateOptions <- names(TSData[-1]) 
+      #dateOptions <- as.Date(strftime(dateOptions, "%m/%d/%y"))
     
       # Refresh every hour
       #invalidateLater(3600000)
-      # Extract time series data
-      #TSData <- get.time.series.data()
       # Save today's date
       lastRefresh <- as.Date(strftime(Sys.time(), "%Y/%m/%d")) - 2
-      lastRefresh <- format(lastRefresh, "%m/%d/%y") 
+      #lastRefresh <- format(lastRefresh, "%m/%d/%y") 
       # Extract all possible dates
-      #dateOptions <- names(TSData[-1])
       #dateOptions <- as.Date(dateOptions, "%m/%d/%y")
       showNotification("Data Refreshed")
   })
   
-  ### Globe plot page
+  ### Map plot page
   # Plot the world map 
   output$world_map <- renderGirafe({
     ggiraph(code = print(get.world.map(type = input$map_data_choice, 
                                        date = input$plot_date, df = TSData)))
   })
   
-  # Plot the JHU data map
-  #output$confirmed_map <- 
-  #  renderGirafe({ggiraph(code = print(confirmed_total_map(
-  #    df = TSData, user_input = input$map_data_choice)))#, date = plot_date))
-  #  ggiraph(code = print(confirmed_total_map(df = TSData, user_input = input$map_data_choice)))
-    # LEAFLET MAP NEEDS WORK
-    #renderLeaflet({
-    #confirmed_total_map(user_input = input$map_data_choice, date = plot_date)
-  #})
-    
-  # Save plot
-  output$download_map_plot <- downloadHandler(
-      filename <- function() {
-        paste("Covid-19_map_", input$map_data_choice, "_", Sys.Date(), ".jpg", sep = "")
-      },
-      content <- function(con) {
-        ggsave(con)
-      }
-    )
+  # Save Globe plot
+  #output$download_map_plot <- downloadHandler(
+  #    filename <- function() {
+  #      paste("Covid-19_map_", input$map_data_choice, "_", Sys.Date(), ".png", sep = "")
+  #    },
+  #    content <- function(file) {
+  #      ggsave(file)
+  #    }
+  #  )
   
   # Save mapping data
   output$download_map_data <- downloadHandler(
     filename = function() {
-      paste("Covid-19_data_", Sys.Date(),'.csv', sep='')
+      paste("Covid-19_data_", Sys.Date(), ".csv", sep = "")
     },
-    content = function(con) {
-      write.csv(dfInput(), con)
+    content = function(file) {
+      write.csv(df, file)
     }
   )
   
   
-  ### Data plot page
+  ### Data page
   dfInput <- reactive({
-    get.df(input$dataFormat, dat, input$map_data_choice, input$dataCountries)})
+    get.df(input$data_format, df, input$map_data_choice, input$data_countries)})
   
-  output$dataToDownload <- renderDataTable(dfInput())
+  output$data_to_download <- renderDataTable(dfInput())
   
-  output$downloadData <- downloadHandler(
+  output$download_data <- downloadHandler(
     filename = function() {
-      paste("Covid-19_data_", Sys.Date(),'.csv', sep='')
+      paste("Covid-19_data_", Sys.Date(),'.csv', sep = '')
     },
-    content = function(con) {
-      write.csv(dfInput(), con)
+    content = function(file) {
+      write.csv(dfInput(), file)
     }
   )
   
   ### Plot page
   plotdfInput <- reactive({
-    get.df("l", dat, input$map_data_choice, input$plotCountries)})
+    get.df1(regions = input$plot_countries, type = input$plot_data_choice,
+            date = input$plot_date, df = df)
+  })
+    #get.df("l", dat, input$map_data_choice, input$plot_countries)})
   
   # Plot specified map
-  output$plot <- renderGirafe({
-    ggiraph(code = print(get.plot(plotName = input$plotTypeChoice, df_stats = plotdfInput())))
+  output$stat_plot <- renderGirafe({
+    ggiraph(code = print(get.plot(plot_name = input$plot_type_choice, df = df)))
   })
   
   # Save plot 
-  output$downloadPlot <- downloadHandler(
-    filename = function(){
-      paste("Covid-19_map_",input$map_data_choice,"_",input$plotTypeChoice, Sys.Date(),".jpg", sep = "")
+  output$download_plot <- downloadHandler(
+    filename = function() {
+      paste("Covid-19_map_", input$map_data_choice, "_", input$plot_type_choice, 
+            Sys.Date(),".png", sep = "")
     },
-    content = function(con) {
-      ggsave(con)
+    content = function(file) {
+      ggsave(file)
     }
   )
   
   # Save plot dataset
-  output$downloadPlotData <- downloadHandler(
-    filename = function(){
-      paste("Covid-19_map_",input$map_data_choice,"_",input$plotTypeChoice, Sys.Date(),".csv", sep = "")
+  output$download_plot_data <- downloadHandler(
+    filename = function() {
+      paste("Covid-19_map_", input$map_data_choice,"_", input$plotTypeChoice, 
+            Sys.Date(),".csv", sep = "")
     },
-    content = function(con) {
-      write.csv(dfInput(), con)
+    content = function(file) {
+      write.csv(dfInput(), file)
     }
   )
   
