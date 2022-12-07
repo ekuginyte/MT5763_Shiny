146f095e70e74fcd2d9bfc8a096370e4376f762a
ekuginyte <ek222@st-andrews.ac.uk>
2022-12-07 23:46:24 +0000

    get.df
    Function get.df was moved to the superseded file.

--- superseded.R
+++ superseded.R
@@ -523,7 +523,43 @@ tabPanel("Current Map", icon = icon("globe"),
 
 
 
+# Plot the JHU data map
+#output$confirmed_map <- 
+#  renderGirafe({ggiraph(code = print(confirmed_total_map(
+#    df = TSData, user_input = input$map_data_choice)))#, date = plot_date))
+#  ggiraph(code = print(confirmed_total_map(df = TSData, user_input = input$map_data_choice)))
+# LEAFLET MAP NEEDS WORK
+#renderLeaflet({
+#confirmed_total_map(user_input = input$map_data_choice, date = plot_date)
+#})
 
 
 
+
+# Dynamic dataset formatter for data page
+get.df <- function(format, df_0, choices, countries){
+  if (!is.null(countries)) {
+    if (!is.null(choices)) {
+      df <- filter(df_0, choice %in% choices) %>%
+        filter(region %in% countries)
+    } else{
+      df <- filter(df_0, region %in% countries)
+    }
+  } else{
+    if (!is.null(choices)) {
+      df <- filter(df_0, choice %in% choices)
+    } else{
+      df <- df_0
+    }
+  }
+  
+  if (is_null(df) == FALSE) {
+    if (format == "w"){
+      df <- pivot_wider(df, names_from = "choice", values_from = "numeric")
+    }
+    return(df)
+  } else {
+    return(df_0)
+  }
+}
 
